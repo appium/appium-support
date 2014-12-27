0a3b83f86c813c88485107b96eb91a839b8a1b51
sebv <seb.vincent@gmail.com>
2014-12-27 21:36:01 +0800

    added tempDir.path method

--- tests/tempdir-specs.js
+++ tests/tempdir-specs.js
@@ -1,16 +1,23 @@
-var tempdir = require('..').tempDir,
+var tempDir = require('..').tempDir,
     fs = require('fs'),
     chai = require('chai');
 
 chai.should();
 
 describe('tempdir', function () {
+  it('should be able to generate a path', function() {
+    var path = tempDir.path({prefix: 'myfile', suffix: '.tmp'});
+    path.should.exists;
+    path.should.include('myfile.tmp');
+  });
+
   it('should be able to create a temp file', function() {
-    return tempdir
+    return tempDir
       .open({prefix: 'my-test-file', suffix: '.zip'})
       .then(function(res) {
         res.should.exist;
         res.path.should.exist;
+        res.path.should.include('my-test-file.zip');
         res.fd.should.exist;
         fs.existsSync(res.path).should.be.ok;
       });
