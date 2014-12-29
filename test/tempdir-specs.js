3d641d57cebcc056e0a50aafe3bda1a3991b1c6c
sebv <seb.vincent@gmail.com>
2014-12-29 17:09:40 +0800

    linted

--- test/tempdir-specs.js
+++ test/tempdir-specs.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var tempDir = require('..').tempDir,
     fs = require('fs'),
     chai = require('chai');
@@ -5,16 +7,16 @@ var tempDir = require('..').tempDir,
 chai.should();
 
 describe('tempdir', function () {
-  it('should be able to generate a path', function() {
+  it('should be able to generate a path', function () {
     var path = tempDir.path({prefix: 'myfile', suffix: '.tmp'});
     path.should.exists;
     path.should.include('myfile.tmp');
   });
 
-  it('should be able to create a temp file', function() {
+  it('should be able to create a temp file', function () {
     return tempDir
       .open({prefix: 'my-test-file', suffix: '.zip'})
-      .then(function(res) {
+      .then(function (res) {
         res.should.exist;
         res.path.should.exist;
         res.path.should.include('my-test-file.zip');
