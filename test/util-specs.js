ed8227c127c8ed9d9c0aecf05fd0d6234595f499
Eric Millin <eric@saucelabs.com>
2015-03-09 11:30:12 -0400

    Added hasContent/Value

---
+++ test/util-specs.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var util = require('..').util
+  , exec = require('child_process').exec
+  , chai = require('chai');
+
+var should = chai.should();
+
+describe('util', function () {
+  var nan = NaN
+    , none = null
+    , f = function () {
+    }
+    , o = {}
+    , n = 0
+    , s = "string"
+    , b = false
+    , e = "";
+
+  describe("hasValue ", function () {
+    it("should work as expected", function () {
+      should.exist(util.hasValue);
+
+      util.hasValue(undefined).should.be.false;
+      util.hasValue(nan).should.be.false;
+      util.hasValue(none).should.be.false;
+      util.hasValue(f).should.be.true;
+      util.hasValue(o).should.be.true;
+      util.hasValue(n).should.be.true;
+      util.hasValue(s).should.be.true;
+      util.hasValue(b).should.be.true;
+    });
+  });
+
+  describe("hasContent ", function () {
+    it("should work as expected", function () {
+      should.exist(util.hasContent);
+
+      util.hasContent(undefined).should.be.false;
+      util.hasContent(nan).should.be.false;
+      util.hasContent(none).should.be.false;
+      util.hasContent(f).should.be.false;
+      util.hasContent(o).should.be.false;
+      util.hasContent(n).should.be.false;
+      util.hasContent(s).should.be.true;
+      util.hasContent(b).should.be.false;
+      util.hasContent(e).should.be.false;
+    });
+  });
+});
