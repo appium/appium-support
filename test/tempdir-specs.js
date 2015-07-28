f2b420694b6da4df6efc0476a68db2552292a29f
Jonathan Lipps <jlipps@gmail.com>
2015-07-28 15:26:11 -0700

    get rid of sync functions

--- test/tempdir-specs.js
+++ test/tempdir-specs.js
@@ -1,7 +1,6 @@
 "use strict";
 
-import { tempDir } from '../..';
-import fs from 'fs';
+import { tempDir, fs } from '../..';
 import chai from 'chai';
 
 chai.should();
@@ -19,24 +18,24 @@ describe('tempdir', function () {
     res.path.should.exist;
     res.path.should.include('my-test-file.zip');
     res.fd.should.exist;
-    fs.existsSync(res.path).should.be.ok;
+    await fs.exists(res.path).should.eventually.be.ok;
   });
 
   it('should generate a random temp dir', async () => {
     let res = await tempDir.openDir();
     res.should.be.a('string');
-    fs.existsSync(res).should.be.ok;
+    await fs.exists(res).should.eventually.be.ok;
     let res2 = await tempDir.openDir();
-    fs.existsSync(res2).should.be.ok;
+    await fs.exists(res2).should.eventually.be.ok;
     res.should.not.equal(res2);
   });
 
   it('should generate one temp dir used for the life of the process', async () => {
     let res = await tempDir.staticDir();
     res.should.be.a('string');
-    fs.existsSync(res).should.be.ok;
+    await fs.exists(res).should.eventually.be.ok;
     let res2 = await tempDir.staticDir();
-    fs.existsSync(res2).should.be.ok;
+    await fs.exists(res2).should.eventually.be.ok;
     res.should.equal(res2);
   });
 });
