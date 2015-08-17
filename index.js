8fe0c9d8784e26368be552455d90a6330f77a616
sebv <seb.vincent@gmail.com>
2015-08-18 00:44:21 +0800

    adds mkdirp multiresolve

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import { cancellableDelay } from './lib/util';
 import fs from './lib/fs';
 import rimraf from './lib/rimraf';
 import * as plist from './lib/plist';
-
+import { mkdirp } from './lib/mkdirp';
 
 // can't add to other exports `as default`
 // until JSHint figures out how to parse that pattern
-export default { tempDir, system, util, fs, rimraf, cancellableDelay, plist };
+export default { tempDir, system, util, fs, rimraf, cancellableDelay, plist, mkdirp };
