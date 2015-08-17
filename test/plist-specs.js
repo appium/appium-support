790cd9f25f41605039223b3a1e1cc1bb3643a18b
Isaac Murchie <isaac@saucelabs.com>
2015-08-17 12:06:14 -0700

    Fix wrong import of fs

--- test/plist-specs.js
+++ test/plist-specs.js
@@ -1,14 +1,31 @@
 import chai from 'chai';
 import path from 'path';
-import { plist } from '../..';
+import { plist, tempDir, fs } from '../..';
 
 
 chai.should();
 
+const plistPath = path.resolve(__dirname, '..', '..', 'test', 'assets', 'sample.plist');
+
 describe('plist', () => {
-  it('parsePlistFile as binary', async () => {
-    let plistPath = path.resolve(__dirname, '..', '..', 'test', 'assets', 'sample.plist');
+  it('should parse plist file as binary', async () => {
     let content = await plist.parsePlistFile(plistPath);
     content.should.have.property('com.apple.locationd.bundle-/System/Library/PrivateFrameworks/Parsec.framework');
+  });
+
+  it.only('should write plist file as binary', async () => {
+    // create a temporary file, to which we will write
+    let plistFile = path.resolve(await tempDir.openDir(), 'sample.plist');
+    await fs.copyFile(plistPath, plistFile);
+
+    // write some data
+    let updatedFields = {
+      'io.appium.test': true
+    };
+    await plist.updatePlistFile(plistFile, updatedFields, true);
+
+    // make sure the data is there
+    let content = await plist.parsePlistFile(plistFile);
+    content.should.have.property('io.appium.test');
   });
 });
