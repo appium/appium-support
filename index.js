8a78b858efe74e35c05ef841b44d28593080358e
Jonah Stiennon <jonahss@gmail.com>
2015-09-10 18:15:06 -0700

    move rimraf fully into fs. cleanup

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import * as system from './lib/system';
 import * as util from './lib/util';
 import { cancellableDelay } from './lib/util';
 import fs from './lib/fs';
-import rimraf from './lib/rimraf';
 import * as plist from './lib/plist';
 import { mkdirp } from './lib/mkdirp';
 
 // can't add to other exports `as default`
 // until JSHint figures out how to parse that pattern
-export default { tempDir, system, util, fs, rimraf, cancellableDelay, plist, mkdirp };
+export default { tempDir, system, util, fs, cancellableDelay, plist, mkdirp };
