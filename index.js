274c602a9055513fe28de9557b575c20aeaf86fd
Jonathan Lipps <jlipps@gmail.com>
2018-01-03 08:34:01 -0800

    add image helpers (#59)
    * Add image-util with methods for cropping images
    
    * remove unnecessary eslint disabling
    
    * address review comments
    
    * change rect intersection logic to match the general notion of intersection
    
    * remove now-erroneous comment

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ import { mkdirp } from './lib/mkdirp';
 import * as logger from './lib/logging';
 import * as process from './lib/process';
 import * as zip from './lib/zip';
+import * as imageUtil from './lib/image-util';
 
 // can't add to other exports `as default`
 // until JSHint figures out how to parse that pattern
 export default { tempDir, system, util, fs, cancellableDelay, plist, mkdirp,
-                 logger, process, zip };
+                 logger, process, zip, imageUtil };
