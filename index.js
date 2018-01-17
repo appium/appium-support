bb351aec043d31c92fe2e98f756ce2bbdc4150eb
Mykola Mokhnach <nick+1@wire.com>
2018-01-17 20:01:46 +0100

    Add helpers for file upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as system from './lib/system';
 import * as util from './lib/util'; // eslint-disable-line import/no-duplicates
 import { cancellableDelay } from './lib/util'; // eslint-disable-line import/no-duplicates
 import fs from './lib/fs';
+import * as net from './lib/net';
 import * as plist from './lib/plist';
 import { mkdirp } from './lib/mkdirp';
 import * as logger from './lib/logging';
@@ -13,4 +14,4 @@ import * as imageUtil from './lib/image-util';
 // can't add to other exports `as default`
 // until JSHint figures out how to parse that pattern
 export default { tempDir, system, util, fs, cancellableDelay, plist, mkdirp,
-                 logger, process, zip, imageUtil };
+                 logger, process, zip, imageUtil, net };
