0b69abc47b996c3ee3265a974d77d6d409f2a50b
Jonathan Lipps <jlipps@gmail.com>
2018-07-05 12:15:44 -0700

    add mjpeg stream helper class

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ import * as logger from './lib/logging';
 import * as process from './lib/process';
 import * as zip from './lib/zip';
 import * as imageUtil from './lib/image-util';
+import * as mjpeg from './lib/mjpeg';
 
 // can't add to other exports `as default`
 // until JSHint figures out how to parse that pattern
 export default { tempDir, system, util, fs, cancellableDelay, plist, mkdirp,
-                 logger, process, zip, imageUtil, net };
+                 logger, process, zip, imageUtil, net, mjpeg };
