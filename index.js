1a32c6b88d5232f61f359c634a1fe3786ca2cd5d
Isaac Murchie <isaac@saucelabs.com>
2018-10-04 10:26:03 -0400

    Fix exports for new Babel

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import * as tempDir from './lib/tempdir';
 import * as system from './lib/system';
-import * as util from './lib/util'; // eslint-disable-line import/no-duplicates
-import { cancellableDelay } from './lib/util'; // eslint-disable-line import/no-duplicates
-import fs from './lib/fs';
+import * as util from './lib/util';
+import * as fsIndex from './lib/fs';
 import * as net from './lib/net';
 import * as plist from './lib/plist';
-import { mkdirp } from './lib/mkdirp';
+import * as mkdirpIndex from './lib/mkdirp';
 import * as logger from './lib/logging';
 import * as process from './lib/process';
 import * as zip from './lib/zip';
 import * as imageUtil from './lib/image-util';
 import * as mjpeg from './lib/mjpeg';
 
+
+const { fs } = fsIndex;
+const { cancellableDelay } = util;
+const { mkdirp } = mkdirpIndex;
 
 export {
   tempDir, system, util, fs, cancellableDelay, plist, mkdirp, logger, process,
