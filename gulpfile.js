4f8a382a1289dbb07d41a075eb7d8c9d9338426f
Isaac Murchie <isaac@saucelabs.com>
2018-11-19 13:31:21 -0500

    Fix errors in coverage generation

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 boilerplate({
   build: 'appium-support',
   coverage: {
-    files: ['./test/**/*-specs.js', '!./test/assets/**'],
-    verbose: true
+    files: [
+      './build/test/**/*-specs.js',
+      '!./build/test/assets/**',
+      '!./build/test/**/*-e2e-specs.js'
+    ],
+    verbose: true,
   },
 });
