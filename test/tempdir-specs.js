2d1848bb422cf1206850c70db36d7d1e45d66770
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-05-01 01:26:38 +0900

    add tmpDir arg for tempdir to respect server tmpdir flag

--- test/tempdir-specs.js
+++ test/tempdir-specs.js
@@ -1,14 +1,23 @@
 
 import { tempDir, fs } from '../index.js';
 import chai from 'chai';
+import nodePath from 'path';
 
 chai.should();
 
 describe('tempdir', function () {
   it('should be able to generate a path', async function () {
-    let path = await tempDir.path({prefix: 'myfile', suffix: '.tmp'});
+    const path = await tempDir.path({prefix: 'myfile', suffix: '.tmp'});
     path.should.exist;
     path.should.include('myfile.tmp');
+  });
+
+  it('should be able to generate a path with tempDirectory', async function () {
+    const preDirPath = await tempDir.staticDir();
+
+    const path = await tempDir.path({prefix: 'myfile', suffix: '.tmp'}, undefined, preDirPath);
+    path.should.exist;
+    path.should.equal(nodePath.join(preDirPath, 'myfile.tmp'));
   });
 
   it('should be able to create a temp file', async function () {
@@ -16,6 +25,17 @@ describe('tempdir', function () {
     res.should.exist;
     res.path.should.exist;
     res.path.should.include('my-test-file.zip');
+    res.fd.should.exist;
+    await fs.exists(res.path).should.eventually.be.ok;
+  });
+
+  it('should be able to create a temp file with tempDirectory', async function () {
+    const preDirPath = await tempDir.staticDir();
+
+    let res = await tempDir.open({prefix: 'my-test-file', suffix: '.zip'}, preDirPath);
+    res.should.exist;
+    res.path.should.exist;
+    res.path.should.equal(nodePath.join(preDirPath, 'my-test-file.zip'));
     res.fd.should.exist;
     await fs.exists(res.path).should.eventually.be.ok;
   });
@@ -36,5 +56,18 @@ describe('tempdir', function () {
     let res2 = await tempDir.staticDir();
     await fs.exists(res2).should.eventually.be.ok;
     res.should.equal(res2);
+  });
+
+  it('should generate one temp dir used for the life of the process with tempDirectory', async function () {
+    const preDirPath = await tempDir.staticDir();
+
+    let res = await tempDir.staticDir(preDirPath);
+    res.should.be.a('string');
+    await fs.exists(res).should.eventually.be.ok;
+    let res2 = await tempDir.staticDir(preDirPath);
+    await fs.exists(res2).should.eventually.be.ok;
+    res.should.equal(res2);
+    res.should.equal(preDirPath);
+    res2.should.equal(preDirPath);
   });
 });
