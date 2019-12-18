b8115aa348aaa2b245a53626d40db52d11956dd9
Isaac A. Murchie <isaac.appium@gmail.com>
2019-12-18 10:39:03 -0500

    refactor: address comments

--- test/timing-specs.js
+++ test/timing-specs.js
@@ -30,70 +30,65 @@ describe('timing', function () {
       }
     });
     it('should get a start time as array', function () {
-      const timer = new timing.Timer();
-      timer.start();
-      _.isArray(timer.startTime).should.be.true;
-    });
-    it('should auto start when requested', function () {
-      const timer = new timing.Timer(true);
+      const timer = new timing.Timer().start();
       _.isArray(timer.startTime).should.be.true;
     });
     it('should get a duration', function () {
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration();
-      _.isNumber(duration).should.be.true;
+      _.isNumber(duration.duration).should.be.true;
+      _.isString(duration.units).should.be.true;
     });
     it('should get correct s', function () {
       processMock.expects('hrtime').twice()
         .onFirstCall().returns([12, 12345])
         .onSecondCall().returns([13, 54321]);
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_SECONDS,
         round: false,
       });
-      duration.should.eql(13.000054321);
+      duration.duration.should.eql(13.000054321);
+      duration.units.should.eql('s');
     });
     it('should get correct s rounded', function () {
       processMock.expects('hrtime').twice()
         .onFirstCall().returns([12, 12345])
         .onSecondCall().returns([13, 54321]);
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_SECONDS,
       });
-      duration.should.eql(13);
+      duration.duration.should.eql(13);
+      duration.units.should.eql('s');
     });
     it('should get correct ms', function () {
       processMock.expects('hrtime').twice()
         .onFirstCall().returns([12, 12345])
         .onSecondCall().returns([13, 54321]);
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_MILLIS,
         round: false,
       });
-      duration.should.eql(13000.054321);
+      duration.duration.should.eql(13000.054321);
+      duration.units.should.eql('ms');
     });
     it('should get correct ns', function () {
       processMock.expects('hrtime').twice()
         .onFirstCall().returns([12, 12345])
         .onSecondCall().returns([13, 54321]);
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_NANOS,
         round: false,
       });
-      duration.should.eql(13000054321);
+      duration.duration.should.eql(13000054321);
+      duration.units.should.eql('ns');
     });
     it('should error if the timer was not started', function () {
       const timer = new timing.Timer();
@@ -102,13 +97,12 @@ describe('timing', function () {
     });
     it('should error if start time is a number', function () {
       const timer = new timing.Timer();
-      timer.startTime = 12345;
+      timer._startTime = 12345;
       expect(() => timer.getDuration())
         .to.throw('Unable to get duration');
     });
     it('should error if passing in wrong unit', function () {
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       expect(() => timer.getDuration('ds'))
         .to.throw('Unknown unit for duration');
     });
@@ -136,56 +130,55 @@ describe('timing', function () {
       }
     }
 
-    it('should get a start time as number', function () {
+    it('should get a duration', function () {
       setupMocks();
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration();
-      _.isNumber(duration).should.be.true;
+      _.isNumber(duration.duration).should.be.true;
     });
     it('should get correct s', function () {
       setupMocks();
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_SECONDS,
         round: false,
       });
-      duration.should.be.eql(10.011483102);
+      duration.duration.should.be.eql(10.011483102);
+      duration.units.should.eql('s');
     });
     it('should get correct s rounded', function () {
       setupMocks();
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_SECONDS,
       });
-      duration.should.be.eql(10);
+      duration.duration.should.be.eql(10);
+      duration.units.should.eql('s');
     });
     it('should get correct ms', function () {
       setupMocks();
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_MILLIS,
         round: false,
       });
-      duration.should.be.eql(10011.483102);
+      duration.duration.should.be.eql(10011.483102);
+      duration.units.should.eql('ms');
     });
     it('should get correct ns', function () {
       setupMocks();
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       const duration = timer.getDuration({
         units: timing.DURATION_NANOS,
         round: false,
       });
-      duration.should.be.eql(10011483102);
+      duration.duration.should.be.eql(10011483102);
+      duration.units.should.eql('ns');
     });
     it('should error if the timer was not started', function () {
       const timer = new timing.Timer();
@@ -194,15 +187,14 @@ describe('timing', function () {
     });
     it('should error if passing in a non-bigint', function () {
       const timer = new timing.Timer();
-      timer.startTime = 12345;
+      timer._startTime = 12345;
       expect(() => timer.getDuration())
         .to.throw('Unable to get duration');
     });
     it('should error if passing in wrong unit', function () {
       setupMocks(true);
 
-      const timer = new timing.Timer();
-      timer.start();
+      const timer = new timing.Timer().start();
       expect(() => timer.getDuration('ds'))
         .to.throw('Unknown unit for duration');
     });
